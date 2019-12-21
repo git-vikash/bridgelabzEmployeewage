827baeb443d562a65e9eb04f90589d9cbb90a800
git-vikash <vikash15081998@gmail.com>
2019-12-21 14:22:48 +0530

    Added:calculate wage for month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -8,8 +8,7 @@ PART_TIME_HOUR=4
 FULL_TIME_HOUR=8
 PART_TIME_EMP=0
 FULL_TIME_EMP=1
-
-#dailyWage=$(( WAGE_PER_HOUR * FULL_DAY_HOUR ))
+DAYS_PER_MONTH=20
 
 checkAttendence=$(( RANDOM%2+1 ));
 
@@ -21,9 +20,11 @@ then
 	
 	case $partOrFullTimeEmp in
 	$PART_TIME_EMP ) dailyWage=$(( WAGE_PER_HOUR * PART_TIME_HOUR ))
-			echo "Part time hour wage:" $dailyWage;;
+			monthWage=$(( dailyWage * DAYS_PER_MONTH ))
+			echo "Part time hour wage:" $monthWage;;
 	$FULL_TIME_EMP ) dailyWage=$(( WAGE_PER_HOUR * FULL_TIME_HOUR ))
-			echo "Full time hour wage:" $dailyWage;;
+			monthWage=$(( dailyWage * DAYS_PER_MONTH ))
+			echo "Full time hour wage:" $monthWage;;
 	esac
 else
 	echo "Employee Absent"
