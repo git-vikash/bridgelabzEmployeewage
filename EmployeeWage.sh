491d63ecf916a19c2f0789a4d6ff40ed890936fc
git-vikash <vikash15081998@gmail.com>
2019-12-21 12:14:36 +0530

    Added: to calculate dailyemployee wages

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,11 +2,19 @@
 
 echo "Welcome To EmployeeWage Simulation"
 
+#CONTANT
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+
+dailyWage=$(( WAGE_PER_HOUR * FULL_DAY_HOUR ))
+
 checkAttendence=$(( RANDOM%2+1 ));
 
 if [[ $checkAttendence -eq 1 ]]
 then
 	echo "Employee Present"
+	echo "Daily wage:"$dailyWage
 else
 	echo "Employee Absent"
 fi
+
