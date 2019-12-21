264f9ecdb1c8f2cec0471747a8a7a46442be61eb
git-vikash <vikash15081998@gmail.com>
2019-12-21 13:14:37 +0530

    Added:using case statement

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -6,15 +6,25 @@ echo "Welcome To EmployeeWage Simulation"
 WAGE_PER_HOUR=20
 PART_TIME_HOUR=4
 FULL_TIME_HOUR=8
+PART_TIME_EMP=0
+FULL_TIME_EMP=1
 
-dailyWage=$(( WAGE_PER_HOUR * FULL_DAY_HOUR ))
+#dailyWage=$(( WAGE_PER_HOUR * FULL_DAY_HOUR ))
 
 checkAttendence=$(( RANDOM%2+1 ));
 
 if [[ $checkAttendence -eq 1 ]]
 then
 	echo "Employee Present"
-	echo "Daily wage:"$dailyWage
+	
+	partOrFullTimeEmp=$(( RANDOM%2 ))
+	
+	case $partOrFullTimeEmp in
+	$PART_TIME_EMP ) dailyWage=$(( WAGE_PER_HOUR * PART_TIME_HOUR ))
+			echo "Part time hour wage:" $dailyWage;;
+	$FULL_TIME_EMP ) dailyWage=$(( WAGE_PER_HOUR * FULL_TIME_HOUR ))
+			echo "Full time hour wage:" $dailyWage;;
+	esac
 else
 	echo "Employee Absent"
 fi
