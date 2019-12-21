04aa0669eb543a2f3c81e8bf248fc913723370ec
git-vikash <vikash15081998@gmail.com>
2019-12-21 11:29:36 +0530

    Add: check employee present or not

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,4 +2,11 @@
 
 echo "Welcome To EmployeeWage Simulation"
 
+checkAttendence=$(( RANDOM%2+1 ));
 
+if [[ $checkAttendence -eq 1 ]]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
